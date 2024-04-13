0327492c48788bcc53147efde638db9813ca7cbe
ayushsingh257 <ayush.singh@kalvium.community>
2024-04-13 08:26:54 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HigherOrderFunction from './Components/HOF'
+
+
+function App() {
+  
+
+  return (
+    <div>      
+      <HigherOrderFunction/>
+    </div>
+  )
+}
+
+export default App
